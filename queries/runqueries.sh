7d246152ac258ac52db999ac5d78d5a0ffd95bf7
kirankumarkolli <kirankk@microsoft.com>
2016-12-12 15:04:06 -0800

    Update runqueries.sh

--- queries/runqueries.sh
+++ queries/runqueries.sh
@@ -2,7 +2,15 @@
 
 for F in `ls query*.sql`;
 do
-    beeline -u "jdbc:hive2://localhost:10001/;transportMode=http" -i /home/sshuser/tpcds-datagen-as-hive-query/queries/settings.hql -f $F &>$F.1.log;
-    beeline -u "jdbc:hive2://localhost:10001/;transportMode=http" -i /home/sshuser/tpcds-datagen-as-hive-query/queries/settings.hql -f $F &>$F.2.log;
-    beeline -u "jdbc:hive2://localhost:10001/;transportMode=http" -i /home/sshuser/tpcds-datagen-as-hive-query/queries/settings.hql -f $F &>$F.3.log;
+    echo START `date` &>$F.1.log
+    beeline -u "jdbc:hive2://localhost:10001/;transportMode=http" -i /home/sshuser/tpcds-datagen-as-hive-query/queries/settings.hql -f $F &>>$F.1.log;
+    echo END `date` &>>$F.1.log
+    
+    echo START `date` &>$F.2.log
+    beeline -u "jdbc:hive2://localhost:10001/;transportMode=http" -i /home/sshuser/tpcds-datagen-as-hive-query/queries/settings.hql -f $F &>>$F.2.log;
+    echo END `date` &>>$F.2.log
+    
+    echo START `date` &>$F.3.log
+    beeline -u "jdbc:hive2://localhost:10001/;transportMode=http" -i /home/sshuser/tpcds-datagen-as-hive-query/queries/settings.hql -f $F &>>$F.3.log;
+    echo END `date` &>>$F.3.log
 done
