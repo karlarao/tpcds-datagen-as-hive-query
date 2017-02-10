3b5a24abc6ea42a313cc480c154a3abe9ebfe5ea
kirankumarkolli <kirankk@microsoft.com>
2017-02-09 18:33:22 -0800

    Fix for compile time

--- queries/summary.sh
+++ queries/summary.sh
@@ -3,7 +3,7 @@ set -e
 #set -o xtrace
 
 # Compile times
-grep --color -e "INFO  : Run DAG" *.sql.*.log | sed 's/.sql.* Query/ /g' | sed 's/s$//g' | awk -F" " '{a[$1]=a[$1]?a[$1]" "$2:$2; mi[$1]=mi[$1]?(mi[$1]>$2?$2:mi[$1]):$2;  ma[$1]=$2>ma[$1]?$2:ma[$1];  }END{for (i in a)print i, a[i] ;}' | sort | showtable -d ' '
+grep --color -e "INFO  : Compile Query" *.sql.*.log | sed 's/.sql.* Query/ /g' | sed 's/s$//g' | awk -F" " '{a[$1]=a[$1]?a[$1]" "$2:$2; mi[$1]=mi[$1]?(mi[$1]>$2?$2:mi[$1]):$2;  ma[$1]=$2>ma[$1]?$2:ma[$1];  }END{for (i in a)print i, a[i] ;}' | sort | showtable -d ' '
 
 # DAG Times
 grep --color -e "INFO  : Run DAG" *.sql.*.log | sed 's/.sql.* DAG/ /g' | sed 's/s$//g' | awk -F" " '{a[$1]=a[$1]?a[$1]" "$2:$2; mi[$1]=mi[$1]?(mi[$1]>$2?$2:mi[$1]):$2;  ma[$1]=$2>ma[$1]?$2:ma[$1];  }END{for (i in a)print i, a[i] ;}' | sort | showtable -d ' '
